1710eef5f1a41ac383f2b86a5b641c1e914b3597
cst-dev-br <felipepcosta.br@gmail.com>
2025-04-04 10:53:23 -0300

    Update script-system.js

--- script-system.js
+++ script-system.js
@@ -4,8 +4,12 @@ var WebsitePath = window.location.pathname;
 var JSONTestURL = 'polite-sky-006c90f10.6.azurestaticapps.net';
 const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
 
-function CheckURL(code) {
-  if (code == JSONTestURL){
+function WebsiteLoad() {
+  if (WebsiteProtocol != 'https://'){
+    location.href = 'https://'+WebsiteURL+WebsitePath;
+  }
+  
+  if (WebsiteURL == JSONTestURL){
     alert('website true');
   } else {
     alert('website false');
