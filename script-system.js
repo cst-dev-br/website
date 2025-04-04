56615e1d44a05f07a906dc6e5e18485772928012
cst-dev-br <felipepcosta.br@gmail.com>
2025-04-04 10:29:37 -0300

    Create script-system.js

---
+++ script-system.js
@@ -0,0 +1,9 @@
+var WebsiteURL = window.location.hostname;
+var WebsiteProtocol = window.location.protocol;
+var WebsitePath = window.location.pathname;
+
+const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
+
+function CheckURL (code) {
+  if (
+}
